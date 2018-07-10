7739f9ed172e31062d266b81f133f3d79211082e
Tyler McNamara <mcnamara14@gmail.com>
2018-07-09 21:26:06 -0600

    Run migration file to add schema to database

--- db/migrations/20180709162438_initial.js
+++ db/migrations/20180709162438_initial.js
@@ -1,22 +1,48 @@
 exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.createTable('restaurants', function(table) {
-        table.increments('id').primary();
-        table.string('name');
+	return Promise.all([
+		knex.schema.createTable('restaurants', function(table) {
+			table.increments('id').primary();
+			table.string('name');
+			table.string('address');
+			table.string('zip');
+			table.string('city');
+			table.string('state');
+			table.integer('phone');
+			table.string('website');
+			table.string('hours');
 
-        table.integer('project_id').unsigned()
-        table.foreign('project_id')
-            .references('projects.id');
+			table.timestamps(true, true);
+		}),
 
-        table.timestamps(true, true);
-        })
-    ])
+		knex.schema.createTable('drinks', function(table) {
+			table.increments('id').primary();
+			table.string('description');
+			table.string('best_deal');
+			table.integer('restaurant_id').unsigned()
+			table.foreign('restaurant_id')
+					.references('restaurants.id');
+
+			table.timestamps(true, true);
+		}),
+
+		knex.schema.createTable('foods', function(table) {
+			table.increments('id').primary();
+			table.string('description');
+			table.string('best_deal');
+			table.integer('restaurant_id').unsigned()
+			table.foreign('restaurant_id')
+					.references('restaurants.id');
+
+			table.timestamps(true, true);
+		})
+	])
 };
-  
-  
+   
 exports.down = function(knex, Promise) {
 	return Promise.all([
-        knex.schema.dropTable('restaurants')
-]);
+		knex.schema.dropTable('restaurants'),
+		knex.schema.dropTable('drinks'),
+		knex.schema.dropTable('foods')
+	]);
 };
   
