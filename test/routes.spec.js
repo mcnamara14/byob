5213a1f9e7323995ed6d8fa3aeb696df21f2b1a9
Camilo Snapp <camilo.snapp@gmail.com>
2018-07-12 12:26:35 -0600

    Added equal testing for GET restaurants

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,29 +5,29 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const knex = require('../db/knex');
-const mockRestaurantData = require('./__mocks__ /restaurants')
+const mockRestaurantData = require('./__mocks__ /restaurants');
 
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     knex.migrate.rollback()
-    .then(function() {
-      knex.migrate.latest()
-      .then(function() {
-        return knex.seed.run()
-        .then(function() {
-          done();
-        });
+      .then(function () {
+        knex.migrate.latest()
+          .then(function () {
+            return knex.seed.run()
+              .then(function () {
+                done();
+              });
+          });
       });
-    });
   });
 
-  afterEach(function(done) {
+  afterEach(function (done) {
     knex.migrate.rollback()
-    .then(function() {
-      done();
-    });
+      .then(function () {
+        done();
+      });
   });
 
   it('should return a response from restaurants', done => {
@@ -35,7 +35,6 @@ describe('Client Routes', () => {
       .get('/api/v1/restaurants')
       .end((err, response) => {
         response.should.have.status(200);
-        // response.res.text.should.equal('We\'re going to test all the routes!');
         done();
       });
   });
@@ -51,24 +50,24 @@ describe('Client Routes', () => {
 });
 
 describe('API Routes', () => {
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     knex.migrate.rollback()
-    .then(function() {
-      knex.migrate.latest()
-      .then(function() {
-        return knex.seed.run()
-        .then(function() {
-          done();
-        });
+      .then(function () {
+        knex.migrate.latest()
+          .then(function () {
+            return knex.seed.run()
+              .then(function () {
+                done();
+              });
+          });
       });
-    });
   });
 
-  afterEach(function(done) {
+  afterEach(function (done) {
     knex.migrate.rollback()
-    .then(function() {
-      done();
-    });
+      .then(function () {
+        done();
+      });
   });
 
   describe('GET /api/v1/restaurants', () => {
@@ -81,20 +80,36 @@ describe('API Routes', () => {
           response.body.should.be.a('array');
           response.body.length.should.equal(20);
           response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
           response.body[0].should.have.property('name');
+          response.body[0].name.should.equal('Ale House');
           response.body[0].should.have.property('address');
+          response.body[0].address.should.equal('2501 16th St');
           response.body[0].should.have.property('zip');
+          response.body[0].zip.should.equal(80211);
           response.body[0].should.have.property('city');
+          response.body[0].city.should.equal('Denver');
           response.body[0].should.have.property('state');
+          response.body[0].state.should.equal('CO');
           response.body[0].should.have.property('phone');
+          response.body[0].phone.should.equal('(303) 433-9734');
           response.body[0].should.have.property('website');
+          response.body[0].website.should.equal('http://www.alehousedenver.com/');
           response.body[0].should.have.property('monday');
+          response.body[0].monday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('tuesday');
+          response.body[0].tuesday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('wednesday');
+          response.body[0].wednesday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('thursday');
+          response.body[0].thursday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('friday');
+          response.body[0].friday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('saturday');
+          // response.body[0].saturday.to.be.null;
+          (response.body[0].saturday === null).should.be.true;
           response.body[0].should.have.property('sunday');
+          (response.body[0].sunday === null).should.be.true;
           done();
         });
     });
@@ -109,21 +124,35 @@ describe('API Routes', () => {
           response.body.should.be.a('array');
           response.body.length.should.equal(1);
           response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(4);
           response.body[0].should.have.property('name');
           response.body[0].name.should.equal('Lodo\'s Bar & Grill');
           response.body[0].should.have.property('address');
+          response.body[0].address.should.equal('1946 Martket St');
           response.body[0].should.have.property('zip');
+          response.body[0].zip.should.equal(80202);
           response.body[0].should.have.property('city');
+          response.body[0].city.should.equal('Denver');
           response.body[0].should.have.property('state');
+          response.body[0].state.should.equal('CO');
           response.body[0].should.have.property('phone');
+          response.body[0].phone.should.equal('(303) 293-8555');
           response.body[0].should.have.property('website');
+          response.body[0].website.should.equal('https://downtown.lodosbarandgrill.com/');
           response.body[0].should.have.property('monday');
+          response.body[0].monday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('tuesday');
+          response.body[0].tuesday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('wednesday');
+          response.body[0].wednesday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('thursday');
+          response.body[0].thursday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('friday');
+          response.body[0].friday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('saturday');
+          response.body[0].saturday.should.equal('3pm - 6pm');
           response.body[0].should.have.property('sunday');
+          (response.body[0].sunday === null).should.be.true;
           done();
         });
     });
@@ -144,5 +173,9 @@ describe('API Routes', () => {
           done();
         });
     });
+  });
+
+  describe('POST /api/v1/drinks', () => {
+
   });
 });
